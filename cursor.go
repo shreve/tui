5a95cc0e24aea41561d4ef96b870013c0698023a
Jacob Evan Shreve <jacob@shreve.io>
2019-12-20 15:29:25 -0500

    Change boolean responses to cursor funcs

--- cursor.go
+++ cursor.go
@@ -6,26 +6,45 @@ type Cursor struct {
 	Row, Col, Height, Width int
 }
 
-func (c *Cursor) Up() {
+func NewCursor(height, width int) Cursor {
+	c := Cursor{}
+	c.SetSize(height, width)
+	return c
+}
+
+func (c *Cursor) Up() bool {
 	if c.Row > 0 {
 		c.Row--;
+		return true;
 	}
+	return false;
 }
 
-func (c *Cursor) Down() {
+func (c *Cursor) Down() bool {
 	if c.Row < c.Height {
 		c.Row++;
+		return true;
 	}
+	return false;
 }
 
-func (c *Cursor) Left() {
+func (c *Cursor) Left() bool {
 	if c.Col > 0 {
 		c.Col--;
+		return true;
 	}
+	return false;
 }
 
-func (c *Cursor) Right() {
+func (c *Cursor) Right() bool {
 	if c.Col < c.Width {
 		c.Col++;
+		return true;
 	}
+	return false;
+}
+
+func (c *Cursor) SetSize(height, width int) {
+	c.Height = height
+	c.Width = width
 }
